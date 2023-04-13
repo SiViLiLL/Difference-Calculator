821e53c570493b44ea077524d8491b8ab18a55d0
Dilyara Arslanova <risharsivil@gmail.com>
2023-04-13 09:35:15 +0300

    feat: add tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,53 @@
+import { test, expect } from '@jest/globals';
+import { readFileSync } from 'node:fs';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+import getDifference from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getAnswer = (answerFileName) => readFileSync(`${resolve(__dirname, '..', '__fixtures__', answerFileName)}`, { encoding: 'utf8', flag: 'r' });
+const getPath = (fileName) => `${resolve(__dirname, '..', '__fixtures__', fileName)}`;
+
+test('comparison of JSON & get stylish', () => {
+  const firstPath = getPath('file1.json');
+  const secondPath = getPath('file2.json');
+
+  expect(getDifference(firstPath, secondPath)).toEqual(getAnswer('answerStylish.txt'));
+});
+
+test('comparsion of Yml & get stylish', () => {
+  const firstPath = getPath('file1.yml');
+  const secondPath = getPath('file2.yml');
+
+  expect(getDifference(firstPath, secondPath)).toEqual(getAnswer('answerStylish.txt'));
+});
+
+test('comparison of JSON & get plain', () => {
+  const firstPath = getPath('file1.json');
+  const secondPath = getPath('file2.json');
+
+  expect(getDifference(firstPath, secondPath, 'plain')).toEqual(getAnswer('answerPlain.txt'));
+});
+
+test('comparsion of Yml & get plain', () => {
+  const firstPath = getPath('file1.yml');
+  const secondPath = getPath('file2.yml');
+
+  expect(getDifference(firstPath, secondPath, 'plain')).toEqual(getAnswer('answerPlain.txt'));
+});
+
+test('comparsion of JSON & get json', () => {
+  const firstPath = getPath('file1.yml');
+  const secondPath = getPath('file2.yml');
+
+  expect(getDifference(firstPath, secondPath, 'json')).toEqual(getAnswer('answerJSON.json'));
+});
+
+test('comparsion of YML & get json', () => {
+  const firstPath = getPath('file1.yml');
+  const secondPath = getPath('file2.yml');
+
+  expect(getDifference(firstPath, secondPath, 'json')).toEqual(getAnswer('answerJSON.json'));
+});
