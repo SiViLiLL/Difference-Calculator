145c6ca7501f93c19f96071f8cd824cbc4049fdc
Dilyara Arslanova <risharsivil@gmail.com>
2023-04-15 13:44:07 +0300

    refactor: stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,40 +1,64 @@
-import { isObj, hasProp, sortWthoutMutation } from '../utilits.js';
+import {
+  isObj, hasProp, sortWthoutMutation, objToString,
+} from '../utilits.js';
 
-const objToString = (obj, level, indent, typeOfIndent) => {
-  const getTotalIndent = (currLevel) => typeOfIndent.repeat(currLevel * indent);
-
-  const iter = (currObj, currLevel) => Object.entries(currObj).reduce((acc, [prop, val]) => {
-    if (isObj(val)) {
-      return `${acc}\n${getTotalIndent(currLevel)}${prop}: {${iter(val, currLevel + 1)}\n${getTotalIndent(currLevel)}}`;
-    }
-    return `${acc}\n${getTotalIndent(currLevel)}${prop}: ${val}`;
-  }, '');
-  return `{${iter(obj, level)}\n${getTotalIndent(level - 1)}}`;
+const getStatusMark = (status) => {
+  switch (status) {
+    case 'added':
+      return ['+ '];
+    case 'removed':
+      return ['- '];
+    case 'updated':
+      return ['+ ', '- '];
+    default:
+      return ['  '];
+  }
 };
 
 export default (diff, indent = 4, typeOfIndent = ' ') => {
   const getTotalIndent = (level, offset) => typeOfIndent.repeat((indent * level) - offset);
 
+  const getStr = (totalIndent, mark, key, val) => `${totalIndent}${mark}${key}: ${val}`;
+
   const iter = (node, level) => `{${sortWthoutMutation(node, (item1, item2) => (item1.key < item2.key))
     .reduce((acc, item) => {
+      const totalInd = getTotalIndent(level, 2);
+      const [mark, oldMark] = getStatusMark(item.status);
+      const totalIndObjKeys = getTotalIndent(level, 0);
+
       if (isObj(item.val) && hasProp(item, 'oldVal')) {
-        return `${acc}\n${getTotalIndent(level, 2)}- ${item.key}: ${item.oldVal}\n${getTotalIndent(level, 2)}+ ${item.key}: ${objToString(item.val, level + 1, indent, typeOfIndent)}`;
+        const oldValStr = getStr(totalInd, mark, item.key, item.oldVal);
+        const objVal = objToString(item.val, true, indent, totalIndObjKeys);
+        const valStr = getStr(totalInd, oldMark, item.key, objVal);
+
+        return `${acc}\n${oldValStr}\n${valStr}`;
       }
       if (isObj(item.val)) {
-        return `${acc}\n${getTotalIndent(level, 2)}${item.status === 'added' ? '+ ' : '- '}${item.key}: ${objToString(item.val, level + 1, indent, typeOfIndent)}`;
+        const objVal = objToString(item.val, true, indent, totalIndObjKeys);
+        const valStr = getStr(totalInd, mark, item.key, objVal);
+        return `${acc}\n${valStr}`;
       }
-      switch (item.status) {
-        case 'tree':
-          return `${acc}\n${getTotalIndent(level, 2)}  ${item.key}: ${iter(item.children, level + 1)}`;
-        case 'not updated':
-          return `${acc}\n${getTotalIndent(level, 2)}  ${item.key}: ${item.val}`;
-        case 'removed':
-          return `${acc}\n${getTotalIndent(level, 2)}- ${item.key}: ${item.val}`;
-        case 'added':
-          return `${acc}\n${getTotalIndent(level, 2)}+ ${item.key}: ${item.val}`;
-        default:
-          return `${acc}\n${getTotalIndent(level, 2)}- ${item.key}: ${isObj(item.oldVal) ? objToString(item.oldVal, level + 1, indent, typeOfIndent) : item.oldVal}\n${getTotalIndent(level, 2)}+ ${item.key}: ${item.val}`;
+
+      if (item.status === 'tree') {
+        const valStr = getStr(totalInd, mark, item.key, iter(item.children, level + 1));
+
+        return `${acc}\n${valStr}`;
       }
+      if (item.status === 'updated') {
+        const valStr = getStr(totalInd, mark, item.key, item.val);
+
+        if (isObj(item.oldVal)) {
+          const oldValObj = objToString(item.oldVal, true, indent, totalIndObjKeys);
+          const oldValStr = getStr(totalInd, oldMark, item.key, oldValObj);
+
+          return `${acc}\n${oldValStr}\n${valStr}`;
+        }
+        const objValStr = getStr(totalInd, oldMark, item.key, item.oldVal);
+
+        return `${acc}\n${objValStr}\n${valStr}`;
+      }
+
+      return `${acc}\n${getStr(totalInd, mark, item.key, item.val)}`;
     }, '')}\n${getTotalIndent(level, indent)}}`;
 
   return iter(diff, 1);
