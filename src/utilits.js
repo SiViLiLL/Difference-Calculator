421052fbcdc471d9c31ac0434f9c673fe82c20f2
Dilyara Arslanova <risharsivil@gmail.com>
2023-04-15 10:57:35 +0300

    feat: add objToString function

--- src/utilits.js
+++ src/utilits.js
@@ -32,4 +32,18 @@ const sortWthoutMutation = (arr, func) => {
   return mergeSort(sortWthoutMutation(leftHalf, func), sortWthoutMutation(rightHalf, func));
 };
 
-export { isObj, hasProp, sortWthoutMutation };
+const objToString = (obj, isNested = false, indent = 4, externalInd = '', typeOfIndent = ' ') => {
+  const getTotalIndent = (currLevel) => externalInd + typeOfIndent.repeat(currLevel * indent);
+
+  const iter = (currObj, currLevel) => Object.entries(currObj).reduce((acc, [prop, val]) => {
+    if (isObj(val)) {
+      return `${acc}\n${getTotalIndent(currLevel)}${prop}: {${iter(val, currLevel + 1)}\n${getTotalIndent(currLevel)}}`;
+    }
+    return `${acc}\n${getTotalIndent(currLevel)}${prop}: ${val}`;
+  }, '');
+  return `${isNested ? '' : externalInd}{${iter(obj, 1)}\n${externalInd}}`;
+};
+
+export {
+  isObj, hasProp, sortWthoutMutation, objToString,
+};
