d35403ff27ba71f8da2b9e5c9031bb439f76a93d
Dilyara Arslanova <risharsivil@gmail.com>
2023-04-16 13:24:36 +0300

    refactor: move getDiff fnc to getDiff.js

---
+++ src/getDiff.js
@@ -0,0 +1,20 @@
+import { hasProp, isObj } from './utilits.js';
+
+const getDiff = (obj1, obj2) => Object.keys({ ...obj1, ...obj2 })
+  .map((key) => {
+    if (hasProp(obj1, key) && hasProp(obj2, key) && obj1[key] === obj2[key]) {
+      return { key, status: 'not updated', val: obj1[key] };
+    }
+    if (hasProp(obj1, key) && hasProp(obj2, key) && isObj(obj1[key]) && isObj(obj2[key])) {
+      return { key, status: 'tree', children: getDiff(obj1[key], obj2[key]) };
+    }
+    if (hasProp(obj1, key) && hasProp(obj2, key)) {
+      return {
+        key, status: 'updated', val: obj2[key], oldVal: obj1[key],
+      };
+    }
+    return hasProp(obj1, key) ? { key, status: 'removed', val: obj1[key] }
+      : { key, status: 'added', val: obj2[key] };
+  });
+
+export default getDiff;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import stylish from './formatters/stylish.js';
 import plain from './formatters/plain.js';
 import json from './formatters/json.js';
-import { hasProp, isObj } from './utilits.js';
 import getParsedContent from './parser.js';
-
-const getDiff = (obj1, obj2) => Object.keys({ ...obj1, ...obj2 })
-  .map((key) => {
-    if (hasProp(obj1, key) && hasProp(obj2, key) && obj1[key] === obj2[key]) {
-      return { key, status: 'not updated', val: obj1[key] };
-    }
-    if (hasProp(obj1, key) && hasProp(obj2, key) && isObj(obj1[key]) && isObj(obj2[key])) {
-      return { key, status: 'tree', children: getDiff(obj1[key], obj2[key]) };
-    }
-    if (hasProp(obj1, key) && hasProp(obj2, key)) {
-      return {
-        key, status: 'updated', val: obj2[key], oldVal: obj1[key],
-      };
-    }
-    return hasProp(obj1, key) ? { key, status: 'removed', val: obj1[key] }
-      : { key, status: 'added', val: obj2[key] };
-  });
+import getDiff from './getDiff.js';
 
 const getFormattedDiff = (format, diff) => {
   switch (format) {
