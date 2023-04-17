db1d0255320fc70af6e3b03afbd40442ec2757bb
Dilyara Arslanova <risharsivil@gmail.com>
2023-04-17 09:43:57 +0300

    refactor: getDiff.js

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,21 +1,19 @@
-import {
-  hasProp, isObj, sortWtMut, getUnKeys,
-} from './utilits.js';
+import _ from 'lodash';
 
-const getDiff = (obj1, obj2) => sortWtMut(getUnKeys(obj1, obj2), (key1, key2) => key1 < key2)
+const getDiff = (obj1, obj2) => _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)))
   .map((key) => {
-    if (hasProp(obj1, key) && hasProp(obj2, key) && obj1[key] === obj2[key]) {
+    if (_.has(obj1, key) && _.has(obj2, key) && obj1[key] === obj2[key]) {
       return { key, status: 'not updated', val: obj1[key] };
     }
-    if (hasProp(obj1, key) && hasProp(obj2, key) && isObj(obj1[key]) && isObj(obj2[key])) {
+    if (_.has(obj1, key) && _.has(obj2, key) && _.isObject(obj1[key]) && _.isObject(obj2[key])) {
       return { key, status: 'tree', children: getDiff(obj1[key], obj2[key]) };
     }
-    if (hasProp(obj1, key) && hasProp(obj2, key)) {
+    if (_.has(obj1, key) && _.has(obj2, key)) {
       return {
         key, status: 'updated', val: obj2[key], oldVal: obj1[key],
       };
     }
-    return hasProp(obj1, key) ? { key, status: 'removed', val: obj1[key] }
+    return _.has(obj1, key) ? { key, status: 'removed', val: obj1[key] }
       : { key, status: 'added', val: obj2[key] };
   });
 
