dbac8a929f7eff2bd90ad1652d26f4fefa40084e
Dilyara Arslanova <risharsivil@gmail.com>
2023-04-18 19:22:18 +0300

    refactor: getDiff

--- src/getDiff.js
+++ src/getDiff.js
@@ -2,13 +2,13 @@ import _ from 'lodash';
 
 const getDiff = (obj1, obj2) => _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)))
   .map((key) => {
+    if (obj1[key] === obj2[key]) {
+      return { key, status: 'not updated', val: obj1[key] };
+    }
+    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+      return { key, status: 'tree', children: getDiff(obj1[key], obj2[key]) };
+    }
     if (_.has(obj1, key) && _.has(obj2, key)) {
-      if (obj1[key] === obj2[key]) {
-        return { key, status: 'not updated', val: obj1[key] };
-      }
-      if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-        return { key, status: 'tree', children: getDiff(obj1[key], obj2[key]) };
-      }
       return {
         key, status: 'updated', val: obj2[key], oldVal: obj1[key],
       };
