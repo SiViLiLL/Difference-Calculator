c7194c6dffcff15d504e4ebd9dcb7e6ebb8015bd
Dilyara Arslanova <risharsivil@gmail.com>
2023-04-19 16:12:25 +0300

    refactor: parser

--- src/parser.js
+++ src/parser.js
@@ -2,12 +2,10 @@ import yaml from 'js-yaml';
 
 export default (data, extName) => {
   switch (extName) {
-    case '.json':
-      return JSON.parse(data);
     case '.yml':
     case '.yaml':
       return yaml.load(data);
     default:
-      throw new Error('This extension isn\'t supported');
+      return JSON.parse(data);
   }
 };
