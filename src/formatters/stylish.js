d7236f7bb6a8af8f787c16d21f6a4767c90b0ab1
Dilyara Arslanova <risharsivil@gmail.com>
2023-04-20 18:41:17 +0300

    refactor: stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -26,36 +26,40 @@ const objToStr = (obj, isNested = false, indent = 4, externalInd = '', typeOfInd
   return `${isNested ? '' : externalInd}{${iter(obj, 1)}\n${externalInd}}`;
 };
 
+const getTotalIndent = (level, indent, typeOfIndent, statusBage = '') => typeOfIndent.repeat((indent * level) - statusBage.length);
+
+const getItemStr = (totalIndent, indent, typeOfIndent, statusBage, key, val) => {
+  if (_.isPlainObject(val)) {
+    const externalIndent = totalIndent + typeOfIndent.repeat(statusBage.length);
+    const objVal = objToStr(val, true, indent, externalIndent);
+
+    return `${totalIndent}${statusBage}${key}: ${objVal}`;
+  }
+  return `${totalIndent}${statusBage}${key}: ${val}`;
+};
+
 export default (diff, indent = 4, typeOfIndent = ' ') => {
-  const getTotalIndent = (level, statusBage = '') => typeOfIndent.repeat((indent * level) - statusBage.length);
-
-  const getItemStr = (totalIndent, statusBage, key, val) => {
-    if (_.isPlainObject(val)) {
-      const externalIndent = totalIndent + typeOfIndent.repeat(statusBage.length);
-      const objVal = objToStr(val, true, indent, externalIndent);
-
-      return `${totalIndent}${statusBage}${key}: ${objVal}`;
-    }
-    return `${totalIndent}${statusBage}${key}: ${val}`;
-  };
-
   const iter = (node, level) => `{${node.reduce((acc, item) => {
     const [statusBage, oldStatusBage] = getStatusBage(item.status);
-    const totalInd = getTotalIndent(level, statusBage);
+    const totalIndent = getTotalIndent(level, indent, typeOfIndent, statusBage);
 
     switch (item.status) {
       case 'tree':
         return `${acc}\n`
-                + `${getItemStr(totalInd, statusBage, item.key, iter(item.children, level + 1))}`;
+                + `${getItemStr(totalIndent, indent, typeOfIndent, statusBage, item.key, iter(item.children, level + 1))}`;
       case 'updated':
         return `${acc}\n`
-                + `${getItemStr(totalInd, oldStatusBage, item.key, item.oldVal)}\n`
-                + `${getItemStr(totalInd, statusBage, item.key, item.val)}`;
-      default:
+                + `${getItemStr(totalIndent, indent, typeOfIndent, oldStatusBage, item.key, item.oldVal)}\n`
+                + `${getItemStr(totalIndent, indent, typeOfIndent, statusBage, item.key, item.val)}`;
+      case 'added':
+      case 'removed':
+      case 'not updated':
         return `${acc}\n`
-                + `${getItemStr(totalInd, statusBage, item.key, item.val)}`;
+                + `${getItemStr(totalIndent, indent, typeOfIndent, statusBage, item.key, item.val)}`;
+      default:
+        return acc;
     }
-  }, '')}\n${getTotalIndent(level - 1)}}`;
+  }, '')}\n${getTotalIndent((level - 1), indent, typeOfIndent)}}`;
 
   return iter(diff, 1);
 };
