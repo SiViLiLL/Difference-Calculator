2b930d8842bcfab9ecd4b95b964e2158c9d238d6
Dilyara Arslanova <risharsivil@gmail.com>
2023-04-20 17:55:59 +0300

    refactor: plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,25 +1,26 @@
 import _ from 'lodash';
 
-export default (diff) => {
-  const convertValToStr = (val) => {
-    if (typeof val === 'string') {
-      return `'${val}'`;
-    }
-    return _.isObject(val) ? '[complex value]' : val;
-  };
+const convertValToStr = (val) => {
+  if (typeof val === 'string') {
+    return `'${val}'`;
+  }
+  return _.isObject(val) ? '[complex value]' : val;
+};
 
+export default (diff) => {
   const iter = (propVal, propName, currAcc) => propVal
     .reduce((acc, item) => {
       const itemVal = convertValToStr(item.val);
-      const itemOldVal = convertValToStr(item.oldVal);
 
       switch (item.status) {
         case 'added':
           return `${acc}Property '${propName + item.key}' was ${item.status} with value: ${itemVal}\n`;
         case 'removed':
           return `${acc}Property '${propName + item.key}' was ${item.status}\n`;
-        case 'updated':
+        case 'updated': {
+          const itemOldVal = convertValToStr(item.oldVal);
           return `${acc}Property '${propName + item.key}' was ${item.status}. From ${itemOldVal} to ${itemVal}\n`;
+        }
         case 'tree':
           return iter(item.children, `${propName + item.key}.`, acc);
         default:
